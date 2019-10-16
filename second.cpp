d516e67b3d9604b839a832f8a4ae828a6fb9d5ec
shongi <sangeetay@iisc.ac.in>
2019-10-16 08:08:55 +0530

    adding second file

---
+++ second.cpp
@@ -0,0 +1,12 @@
+// A simple program that computes the square root of a number
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+
+     cout << "Hello World from second file" << endl;
+
+     return(0);
+
+}
